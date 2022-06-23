599c92f05c09ed7fb285257d3eb42b1e9d4bb87d
James Goldie <james@dicenerd.app>
2022-06-23 11:53:09 +1000

    Convert docs to MD

--- R/main.r
+++ R/main.r
@@ -1,24 +1,29 @@
 #' Label facets with a string template.
 #'
-#' \code{label_glue} returns a labeller function that you can give to the
-#' \code{labeller} argument of a \code{facet_*} function. If you're using
-#' \code{label_glue} with \code{facet_wrap} or you're individually supplying
-#' labellers to each variable, you only need one string template. If you're
-#' using it with \code{facet_grid} directly, you need to give two templates:
-#' \code{rows} and \code{cols}.
+#' Returns a labeller function that you can give to the `labeller` argument of
+#' a `facet_*` function.
+#' 
+#' If you're using [label_glue()] with [ggplot2::facet_wrap()] or you're
+#' individually supplying labellers to each variable, you only need one string
+#' template: `row_template`.
+#' 
+#' If you're using it with [ggplot2::facet_grid()], you need to supply two
+#' templates: one for the rows (`row_template`) and one for the columns
+#' (`col_template`).
 #'
-#' If you're using the labeller with \code{facet_wrap}, you can also use these
-#' variables in \code{glue} strings:
-#' \itemize{
-#'   \item \code{.n} to add numbers to each facet;
-#'   \item \code{.l} or \code{.L} to add lower- or uppercase letters
-#'   \item \code{.r} or \code{.R} to add lower or uppercase roman numerals.
+#' If you're using the labeller with [`ggplot2::facet_wrap()`], you can also
+#' use these variables in the templates:
+#'   * `.n` to add numbers to each facet;
+#'   * `.l` or `.L` to add lower- or uppercase letters
+#'   * `.r` or `.R` to add lower- or uppercase roman numerals.
 #' }
-#' @param rows A string to be used as the template by \code{glue}.
-#' @param cols A string to be used as the template by \code{glue}.
+#' @param row_template A string to be used as the template by
+#' [glue::glue_data()].
+#' @param col_template A string to be used as the template by
+#' [glue::glue_data()].
 #'
-#' @return A labelling function that you can give to the \code{labeller} argument
-#'   of a \code{facet_*} function.
+#' @return A labelling function that you can give to the `labeller` argument
+#'   of the facetting function.
 #' @examples
 #' library(ggplot2)
 #' library(stickylabeller)
@@ -27,21 +32,21 @@
 #' p1 <- ggplot(iris, aes(x = Sepal.Length, y = Petal.Length)) + geom_point()
 #' p1 + facet_wrap(
 #'   ~  Species,
-#'   labeller = label_glue('Sepal and petal lengths in {Species} plants'))
+#'   labeller = label_glue("Sepal and petal lengths in {Species} plants"))
 #'
 #' # distinguish panels with .n (numbers), .l (lowercase), .L (uppercase),
 #' # .r or .R (lower- or uppercase roman) if you're using facet_wrap
 #' p1 + facet_wrap(
 #'   ~  Species,
-#'   labeller = label_glue('({.n}) {Species}'))
+#'   labeller = label_glue("({.n}) {Species}"))
 #'
 #' # you can also use label_glue with facet_grid
 #' p2 <- ggplot(mtcars, aes(x = disp, y = mpg)) + geom_point()
 #' p2 + facet_grid(
 #'   gear ~ cyl,
 #'   labeller = label_glue(
-#'     rows = '{gear} gears',
-#'     cols = '{cyl} cylinders'))
+#'     row_template = "{gear} gears",
+#'     col_template = "{cyl} cylinders"))
 #'
 #' # you can add summary statistics in a couple of ways. the easiest (in terms
 #' # of plot code) is to join a summary back into the original data and to add
@@ -49,18 +54,18 @@
 #' library(dplyr)
 #' cyl_stats <- mtcars %>%
 #'   group_by(cyl) %>%
-#'   summarise(cyl_n = n(), cyl_meanmpg = sprintf('%#.2f', mean(mpg)))
+#'   summarise(cyl_n = n(), cyl_meanmpg = sprintf("%#.2f", mean(mpg)))
 #' mtcars_joined <- mtcars %>% inner_join(cyl_stats)
 #'
 #' p3 <- ggplot(mtcars_joined, aes(x = disp, y = mpg)) + geom_point()
 #' p3 + facet_wrap(
 #'   ~ cyl + cyl_n + cyl_meanmpg,
 #'   labeller = label_glue(
-#'     '({.l}) {cyl} cylinders\n(n = {cyl_n}, mean = {cyl_meanmpg})'))
+#'     "({.l}) {cyl} cylinders\n(n = {cyl_n}, mean = {cyl_meanmpg})"))
 #'
 #'
 #' @export
-label_glue <- function(rows, cols) {
+label_glue <- function(row_template, col_template, summary_data) {
 
   # here's the inner function. label_glue returns this, but it can access the
   # template string given when the user calls label_glue
@@ -73,7 +78,7 @@ label_glue <- function(rows, cols) {
       # for facet_wrap, convert incoming labels to strings
       # and add extra columns for numbering
       facet_count <- nrow(labels)
-      template <- rows
+      template <- row_template
       labels <- lapply(labels, as.character)
       labels[[".n"]] <- as.character(seq_len(facet_count))
       labels[[".l"]] <- make_letters(seq_len(facet_count))
@@ -83,7 +88,7 @@ label_glue <- function(rows, cols) {
 
     } else if (!is.null(facet_type) & facet_type == "grid") {
 
-      if (numbering_present(cols) | numbering_present(rows)) {
+      if (numbering_present(col_template) | numbering_present(row_template)) {
         stop(paste("Error: the column or row label contains .n, .l or .L.",
           "label_glue can currently only number the facets in facet_wrap.",
           "For more info, see",
@@ -91,10 +96,10 @@ label_glue <- function(rows, cols) {
       }
 
       facet_direction <- attr(labels, "type")
-      if (facet_direction == "rows") {
-        template <- rows
-      } else if (facet_direction == "cols") {
-        template <- cols
+      if (facet_direction == "row_template") {
+        template <- row_template
+      } else if (facet_direction == "col_template") {
+        template <- col_template
       } else {
         stop(paste("Error: unrecognised facet_direction in label_guide. This",
           "is probably a bug in stickylabeller. Please report it to",
@@ -107,13 +112,13 @@ label_glue <- function(rows, cols) {
       # if no facet type is specified (eg. inside labeller wrapper), just do
       # the basics
 
-      if (numbering_present(rows)) {
+      if (numbering_present(row_template)) {
         stop(paste("Error: the column or row label contains .n, .l or .L.",
           "label_glue can currently only number the facets in facet_wrap.",
           "For more info, see",
           "https://github.com/rensa/stickylabeller/issues/1"))
       }
-      template <- rows
+      template <- row_template
       labels = lapply(labels, as.character)
     }
 
